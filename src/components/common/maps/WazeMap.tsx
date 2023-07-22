c185f93bda4c8eb44ba12e2de76189d4c4d1457e
acrichards3 <alexchristopherrichards@gmail.com>
2023-07-22 15:15:46 -0400

    wazeMap

---
+++ src/components/common/maps/WazeMap.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function WazeMap() {
+  return (
+    <iframe
+      src="https://embed.waze.com/iframe?zoom=12&lat=45.6906304&lon=-120.810983"
+      width="100%"
+      height="100%"
+    ></iframe>
+  );
+}
