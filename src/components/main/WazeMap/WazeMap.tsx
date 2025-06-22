dea61bcd2bab23d6b69a5f17d26fa261da942732
Alex Richards <alexchristopherrichards@gmail.com>
2025-06-22 16:42:01 -0400

    fix

--- src/components/main/WazeMap/WazeMap.tsx
+++ src/components/main/WazeMap/WazeMap.tsx
@@ -12,7 +12,7 @@ export const WazeMap: React.FC<WazeMapProps> = (props) => {
   const { currentLat, currentLon, previousLat, previousLon } = props;
   const angle = Math.atan2(currentLat - previousLat, currentLon - previousLon) * (180 / Math.PI);
   const mapAngle = angle < 0 ? angle + 360 : angle;
-  const adjustedAngle = (mapAngle + 90) % 360;
+  const adjustedAngle = (mapAngle - 90) % 360;
   return (
     <div className={styles.container} style={{ transform: `rotate(${adjustedAngle}deg)` }}>
       <iframe
