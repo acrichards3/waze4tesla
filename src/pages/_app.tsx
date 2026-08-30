0458ef3838a5e0827c89df62f33ba8692e460467
Alex Richards <alexchristopherrichards@gmail.com>
2026-08-30 13:30:32 -0400

    fix font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,10 @@
 import { type AppType } from 'next/app';
 import { api } from '~/utils/api';
-import { Roboto_Condensed } from 'next/font/google';
 import '~/styles/globals.css';
-
-const robotoCondensed = Roboto_Condensed({
-  weight: ['300', '400', '700'],
-  style: 'normal',
-  subsets: ['latin'],
-});
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <main className={robotoCondensed.className}>
+    <main>
       <Component {...pageProps} />
     </main>
   );
